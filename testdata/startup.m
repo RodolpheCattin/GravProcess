3ebd125d407f0b1f0cf8ccb5c2aac5d2aaef2293
Rodolphe Cattin <58222141+RodolpheCattin@users.noreply.github.com>
2021-04-18 18:02:09 +0200

    Add files via upload

---
+++ testdata/startup.m
@@ -0,0 +1,5 @@
+%% path to use GravProcess 1.3
+gravprocesspath = '..';
+addpath(path,[gravprocesspath,'/gravprocess']);
+addpath(path,[gravprocesspath,'/terrain']);
+addpath(path,[gravprocesspath,'/terrain/Mesh2dv24']);
